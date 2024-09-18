3abdcec3568aec5b7e06b4fdb1e0ad308664a531
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2024-09-18 03:14:12 -0400

    Update SerializedTest.java

--- src/test/java/com/github/davidmoten/rx/SerializedTest.java
+++ src/test/java/com/github/davidmoten/rx/SerializedTest.java
@@ -1,51 +1,54 @@
 package com.github.davidmoten.rx;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.util.List;
-
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
 import org.junit.Test;
 
-import rx.Observable;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.fail;
 
 public class SerializedTest {
 
-    @Test
-    public void testSerializeUnregisteredClass() {
-        File file = new File("target/temp_unregistered");
-        file.delete();
-        // Create an instance of an unregistered class
-        UnregisteredClass obj = new UnregisteredClass("test");
-        try {
-            Serialized.kryo().write(Observable.just(obj), file).subscribe();
-        } catch (Exception e) {
-            // If an exception occurs, fail the test
-            fail("Serialization failed: " + e.getMessage());
+    /**
+     * An example unregistered class to serialize.
+     */
+    public static class UnregisteredClass {
+        private String data;
+
+        public UnregisteredClass(String data) {
+            this.data = data;
         }
-        assertTrue(file.exists());
-        assertTrue(file.length() > 0);
-        // Try to deserialize
-        List<UnregisteredClass> list = Serialized.kryo()
-                .read(UnregisteredClass.class, file)
-                .toList()
-                .toBlocking()
-                .single();
-        assertEquals(1, list.size());
-        assertEquals("test", list.get(0).name);
+
+        // Getter and setter methods
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
     }
 
-    static class UnregisteredClass {
-        final String name;
+    @Test
+    public void testSerializeUnregisteredClass() {
+        Kryo kryo = new Kryo();
+        // Note: In Kryo v3.0.3, registrationRequired is false by default.
+        // In Kryo v5.0.0-RC4, registrationRequired is true by default.
 
-        UnregisteredClass() {
-            this("");
-        }
+        UnregisteredClass obj = new UnregisteredClass("test data");
 
-        UnregisteredClass(String name) {
-            this.name = name;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Output output = new Output(baos);
+
+        try {
+            kryo.writeObject(output, obj);
+            output.close();
+            // In v3.0.3, this succeeds because registration is not required.
+            // In v5.0.0-RC4, this throws an exception because registration is required.
+        } catch (Exception e) {
+            // Exception is expected in v5.0.0-RC4 due to the change in default behavior.
+            fail("Exception was thrown during serialization: " + e.getMessage());
         }
     }
 }
